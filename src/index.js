64d1d4403e525336ef11c99f924dd1d602ddaec2
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-02-24 00:33:55 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-// app.engine('handlebars', handlebars());
+// app.engine('hbs', handlebars({
 app.engine("hbs", engine({
   extname: '.hbs'
 }));
