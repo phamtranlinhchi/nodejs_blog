08fb1dcfe2f2d9bb0692f0f9225e09a1b5b6938c
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-02-23 21:24:51 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
-    var a=1;
-  var b=2;
-  var c = a + b;
-  res.send('Hello World!')
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1 style="color: red;">Hello World</h1>
+  `)
 })
 
 app.listen(port, () => {
