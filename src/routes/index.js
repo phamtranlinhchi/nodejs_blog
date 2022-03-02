bc316901de2aa5c180780bae6a8cab2f9730e48e
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-03-03 02:08:31 +0700

    update

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+function route(app) {
+    // app.get('/', (req, res) => {
+    //     res.render('home');
+    // });
+
+    // app.get('/news', (req, res) => {
+    //     res.render('news');
+    // });
+
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
+
+    // app.get('/search', (req, res) => {
+    //     res.render('search');
+    // });
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send('');
+    // });
+}
+
+module.exports = route;
